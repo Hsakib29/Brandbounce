c968630ea4d6cdcd7905888b15d147b706134e40
Mehdi Hasan Sakib <106773301+Hsakib29@users.noreply.github.com>
2025-05-19 00:08:22 +0600

    made changes to postcss.config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,27 +1,14 @@
 /** @type {import('postcss-load-config').Config} */
+
 const config = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          require('@fullhuman/postcss-purgecss')({
-            content: [
-              './app/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
-              './styles/**/*.{js,jsx,ts,tsx}', // If you have JS/TS files with Tailwind classes
-            ],
-            safelist: [
-              // Add any classes that might be dynamically added or not detected by PurgeCSS
-              // Example: 'active', 'show', 'hidden'
-            ],
-          }),
-          require('cssnano')({
-            preset: 'default',
-          }),
-        ]
-      : []),
-  ],
+
+  plugins: {
+
+    tailwindcss: {},
+
+  },
+
 };
+
 
 export default config;
